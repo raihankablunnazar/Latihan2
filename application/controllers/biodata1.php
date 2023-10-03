30018ade9c26e457eac0f4a392613573b0177194
raihankablunnazar <raihankablunn@gmail.com>
2023-10-03 17:21:57 +0700

    menambahkan matakuliah

---
+++ application/controllers/biodata1.php
@@ -0,0 +1,12 @@
+<?php
+class biodata1 extends CI_controller
+{
+    public function index()
+    {
+        echo"<h1> Perkenalkan</h1>";
+        echo"Nama saya Aliif Musyaffa Zahraan
+        Nim:19220074
+        Saya tinggal di Jakarta pusat
+        olahraga kesukaan sepak bola";
+    }
+}
